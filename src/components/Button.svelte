d4ae30bf164435987858901f5f4b7ccf38062c95
AlexGonRod <alexgonrod83@gmail.com>
2025-02-25 16:35:37 +0100

    chore: put own styles

--- src/components/Button.svelte
+++ src/components/Button.svelte
@@ -3,3 +3,15 @@
 </script>
 
 <button {...rest} {onclick}>{@render children()}</button>
+
+<style>
+	button {
+		background-color: inherit; 
+		border: none;
+		color: #111827;
+		text-align: left;
+		text-decoration: none;
+		flex-grow: 1;;
+		font-size: 15px;
+	}
+</style>
