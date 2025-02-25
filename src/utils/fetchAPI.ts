ec6ae7198dcc0675a0262e443bfddcbb4a3211de
AlexGonRod <alexgonrod83@gmail.com>
2025-02-25 13:32:06 +0100

    feat:new prompt fetchAPI

--- src/utils/fetchAPI.ts
+++ src/utils/fetchAPI.ts
@@ -1,30 +1,24 @@
+import { Gemini } from "./gemini";
+
 type Res = {
 	error?: string | null;
-	messages: string[]
+	messages: string[] | string
 }
-export async function fetchAPI(type: string | null = '', value: string[] | string) {
+
+export async function fetchAPI(value: string, type: string | null = null) {
 	let response: Res = { error: null, messages: [] }
-	console.log("value: ", value)
 	try {
-		let prompt: string = '';
-		if (type) {
-			prompt = `Please read this ${value} and rephrase its sentences in Spanish using this JSON schema:
-			message = {'message': string}
-			Return: Array<message>`
-		 }
 		
-		prompt = `Please rephrase this sentences in Spanish: ${value} using this JSON schema:
-			message = {'message': string}
-			Return: Array<message>`;
-		const result = await new Gemini(prompt).setResponse();
+		const result = await new Gemini(value, type).setResponse();
+		
 		const res = result.response.text()
 		const json = res.replace(/```json|```/g, '').trim()
 		response.messages = JSON.parse(json)
+
 		return response
-		
+
 	} catch (err) {
 		response.error = (err as any)?.message
 		return response
 	}
 }
-
