8dfcf4757319e9f2942ec5f4e3593082d3465078
AlexGonRod <alexgonrod83@gmail.com>
2025-02-25 16:35:52 +0100

    fix: styles

--- src/components/Loader.svelte
+++ src/components/Loader.svelte
@@ -2,38 +2,43 @@
 
 <style>
 	.loader {
-  width: 8px;
-  height: 40px;
-  border-radius: 4px;
-  display: block;
-  margin: 20px auto;
-  position: relative;
-  background: currentColor;
-  color: #FFF;
-  box-sizing: border-box;
-  animation: animloader 0.3s 0.3s linear infinite alternate;
-}
+		width: 8px;
+		height: 40px;
+		border-radius: 4px;
+		display: block;
+		margin: 40px auto;
+		position: relative;
+		background: currentColor;
+		color: #6b7280;
+		box-sizing: border-box;
+		animation: animloader 0.3s 0.3s linear infinite alternate;
+	}
 
-.loader::after, .loader::before {
-  content: '';
-  width: 8px;
-  height: 40px;
-  border-radius: 4px;
-  background: currentColor;
-  position: absolute;
-  top: 50%;
-  transform: translateY(-50%);
-  left: 20px;
-  box-sizing: border-box;
-  animation: animloader 0.3s  0.45s  linear infinite alternate;
-}
-.loader::before {
-  left: -20px;
-  animation-delay: 0s;
-}
+	.loader::after,
+	.loader::before {
+		content: "";
+		width: 8px;
+		height: 40px;
+		border-radius: 4px;
+		background: currentColor;
+		position: absolute;
+		top: 50%;
+		transform: translateY(-50%);
+		left: 20px;
+		box-sizing: border-box;
+		animation: animloader 0.3s 0.45s linear infinite alternate;
+	}
+	.loader::before {
+		left: -20px;
+		animation-delay: 0s;
+	}
 
-@keyframes animloader {
-  0%   { height: 48px} 
-  100% { height: 4px}
-}
+	@keyframes animloader {
+		0% {
+			height: 48px;
+		}
+		100% {
+			height: 4px;
+		}
+	}
 </style>
