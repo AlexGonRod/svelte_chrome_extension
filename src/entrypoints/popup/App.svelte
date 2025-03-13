dc5244ef54fbcd20424fd8d845c7a12477e79ae8
AlexGonRod <alexgonrod83@gmail.com>
2025-03-13 11:35:32 +0100

    feat: adding features in popup

--- src/entrypoints/popup/App.svelte
+++ src/entrypoints/popup/App.svelte
@@ -5,6 +5,7 @@
 	import CopyClipboard from "../../components/CopyClipboard.svelte";
 	let id = $state(getTab());
 	let loading = $state(false);
+	const pressBack = document.addEventListener("keydown", (e) => e.key === 'Escape')
 
 	async function getTab(): Promise<number> {
 		const [tab] = await chrome.tabs.query({
@@ -31,8 +32,10 @@
 			action: "stopSelecting",
 			id,
 		});
+		loading = true;
 		const res = await fetchAPI(selectedTexts);
 		texts = res;
+		loading = false
 	}
 
 	async function capture() {
@@ -54,7 +57,7 @@
 				);
 			});
 
-			if (!response.ready) {
+			if (!response?.ready || pressBack ) {
 				console.log("[popup.js] Ya hay una captura en progreso");
 				// Opcionalmente, puedes cancelar la captura actual
 				await new Promise((resolve) => {
@@ -104,55 +107,70 @@
 		<div class="logo-container">
 			<div class="logo">
 				<!-- SVG del logo -->
-				<svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-					<path d="M4 6H20L18 18H6L4 6Z" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-					<path d="M8 20H16" stroke="white" stroke-width="2" stroke-linecap="round"/>
+				<svg
+					viewBox="0 0 24 24"
+					fill="none"
+					xmlns="http://www.w3.org/2000/svg"
+				>
+					<path
+						d="M4 6H20L18 18H6L4 6Z"
+						stroke="white"
+						stroke-width="2"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+					/>
+					<path
+						d="M8 20H16"
+						stroke="white"
+						stroke-width="2"
+						stroke-linecap="round"
+					/>
 				</svg>
 			</div>
 			<span class="logo-text">Dropprice</span>
 		</div>
-	</div> 
-			
-	 <div class="main-container">
+	</div>
+
+	<div class="main-container">
 		<div class="tool-button" id="select-tool">
-            <Button onclick={send}>Seleccionar Elemento</Button>
-            <div class="tool-shortcut">
-                <i>⌘</i>
-            </div>
-        </div>
+			<Button onclick={send}>Seleccionar Elemento</Button>
+			<div class="tool-shortcut">
+				<i>⌘</i>
+			</div>
+		</div>
 		<div class="tool-button" id="stop-tool">
-            <Button onclick={recieve}>Detener Selección</Button>
-            <div class="tool-shortcut">
-                <i>⏹</i>
-            </div>
-        </div>
+			<Button onclick={recieve}>Detener Selección</Button>
+			<div class="tool-shortcut">
+				<i>⏹</i>
+			</div>
+		</div>
 		<div class="tool-button" id="capture-tool">
-            <Button onclick={capture}>Capturar</Button>
-            <div class="tool-shortcut">
-                <i>+</i>
-            </div>
-        </div>
+			<Button onclick={capture}>Capturar</Button>
+			<div class="tool-shortcut">
+				<i>+</i>
+			</div>
+		</div>
 
 		{#if texts && texts?.messages?.length > 0 && !loading}
-		<div class="tool-button" id="copy-tool">
-				<CopyClipboard  tag={"li"} />
-            <div class="tool-shortcut">
-                <i>+</i>
-            </div>
-        </div>
+			<div class="tool-button" id="copy-tool">
+				<CopyClipboard tag={"li"} />
+				<div class="tool-shortcut">
+					<i>+</i>
+				</div>
+			</div>
 		{/if}
 		<div class="content">
 			{#if loading}
-			<Loader />
+				<Loader />
 			{:else}
-			<ul>
-				{#if texts.error}
-					<li><p>{texts.error}</p></li>
-				{/if}
-				{#each texts.messages as text}
-					<li>{text.message}</li>
-				{/each}
-			</ul>
+				<ul>
+					{#if texts.error}
+						<li><p>{texts.error}</p></li>
+					{/if}
+					{#each texts.messages as text}
+						<li>{text.message}</li>
+					{/each}
+				</ul>
 			{/if}
 		</div>
 	</div>
